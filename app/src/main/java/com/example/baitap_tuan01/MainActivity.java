676881da84dd348cb0317f1293fe155d66b651ff
NguyenHuuThang <thangtb100@gmail.com>
2021-04-27 22:06:09 +0700

    bttuan1 (sua lai mail)

---
+++ app/src/main/java/com/example/baitap_tuan01/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.baitap_tuan01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_01);
+    }
+}
